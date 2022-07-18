193955f4225175c06d90c4294624232ceb91448f
Zach Dearmin <zacharydearmin@gmail.com>
2022-07-18 10:56:04 -0400

    Updated MovieAppearanceService classe

--- MarvelAPI.Services/MovieAppearance/IMovieAppearanceService.cs
+++ MarvelAPI.Services/MovieAppearance/IMovieAppearanceService.cs
@@ -1,7 +1,10 @@
+using System.Threading.Tasks;
+using MarvelAPI.Models.MovieAppearance;
+
 namespace MarvelAPI.Services.MovieAppearance
 {
     public interface IMovieAppearanceService
     {
-         
+        Task<bool> CreateMovieAppearanceAsync(MovieAppearanceCreate model);
     }
 }
