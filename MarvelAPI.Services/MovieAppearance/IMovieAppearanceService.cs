0a2252cfc4e83a4721a03d0bdde5a362244d0f06
Zach Dearmin <zacharydearmin@gmail.com>
2022-07-18 11:49:50 -0400

    CRUD methods added to MovieAppearanceServices

--- MarvelAPI.Services/MovieAppearance/IMovieAppearanceService.cs
+++ MarvelAPI.Services/MovieAppearance/IMovieAppearanceService.cs
@@ -1,10 +1,16 @@
 using System.Threading.Tasks;
+using MarvelAPI.Data;
+using MarvelAPI.Data.Entities;
+using MarvelAPI.Models;
 using MarvelAPI.Models.MovieAppearance;
 
 namespace MarvelAPI.Services.MovieAppearance
 {
     public interface IMovieAppearanceService
     {
-        Task<bool> CreateMovieAppearanceAsync(MovieAppearanceCreate model);
+        Task<bool> CreateMovieAppearanceAsync(MovieAppearanceCreate request);
+        Task<IEnumerable<MovieAppearanceEntity>> GetAllMovieAppearancesAsync();
+        Task<bool> UpdateMovieAppearanceAsync(MovieAppearanceDetail request);
+        Task<bool> DeleteMovieAppearanceAsync(int id);
     }
 }
