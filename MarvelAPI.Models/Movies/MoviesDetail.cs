2227fd43e131818116a1a05630fe4f99b9e44537
Mary Giles Beeson <marybeeson23@gmail.com>
2022-07-18 07:19:39 -0400

    movies models added

---
+++ MarvelAPI.Models/Movies/MoviesDetail.cs
@@ -0,0 +1,11 @@
+// TODO Add usings
+
+namespace MarvelAPI.Models.Movies
+{
+    public class MoviesDetail
+    {
+        public int Id { get; set; }   
+        public string Title { get; set; }
+        public DateTime ReleaseYear { get; set; }
+    }
+}
