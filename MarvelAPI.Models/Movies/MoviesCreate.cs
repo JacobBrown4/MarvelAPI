e0dfec08583bcf2bda23ebbf7073d2c5aff5f8a8
Zach Dearmin <zacharydearmin@gmail.com>
2022-07-20 14:58:25 -0400

    Updated the movies model assembly

--- MarvelAPI.Models/Movies/MoviesCreate.cs
+++ MarvelAPI.Models/Movies/MoviesCreate.cs
@@ -5,7 +5,7 @@ namespace MarvelAPI.Models.Movies
     public class MoviesCreate
     {
         [Required]
-        [MinLength(10, ErrorMessage = "{0} must be at least {1} characters long.")]
+        [MinLength(1, ErrorMessage = "{0} must be at least {1} characters long.")]
         [MaxLength(150, ErrorMessage = "{0} must be no more than {1} characters long.")]
         public string Title { get; set; }
     }
