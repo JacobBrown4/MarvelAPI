21e1ed7947cb369b21d525b07a4199c0f2d9af35
Zach Dearmin <zacharydearmin@gmail.com>
2022-07-25 13:32:01 -0400

    Added framework for UserController

---
+++ MarvelAPI.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MarvelAPI.Services.User;
+
+namespace MarvelAPI.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _service;
+        public UserController(IUserService service)
+        {
+            _service = service;
+        }
+    }
+}
