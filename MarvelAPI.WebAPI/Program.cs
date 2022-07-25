a04643c294cbe5a8a3c3c7d1e1117b90debb777f
Nick Andresen <nimandresen@gmail.com>
2022-07-25 16:09:53 -0400

    Progress following through Canvas module 13.06

--- MarvelAPI.WebAPI/Program.cs
+++ MarvelAPI.WebAPI/Program.cs
@@ -11,6 +11,7 @@ using Microsoft.Data.SqlClient;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using MarvelAPI.Services.Token;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -67,7 +68,44 @@ builder.Services.AddAuthentication
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "MarvelAPI.WebAPI", Version = "v1"
+        });
+        c.AddSecurityDefinition
+        (
+            "Bearer", new OpenApiSecurityScheme()
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "JWT Authorization header using the Bearer scheme. \n\n Enter 'Bearer' [space] and then your token in the text input below.\n\nExample: \"Bearer 12345abcdef\""
+            }
+        );
+        c.AddSecurityRequirement
+        (
+            new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] {}
+                }
+            }
+        );
+    }
+);
 
 var app = builder.Build();
 
